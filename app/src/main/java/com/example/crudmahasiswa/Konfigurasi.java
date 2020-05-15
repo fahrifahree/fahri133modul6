e51d6f7bb107f43f5d9afb70b674cdc327461ee1
ddani085 <indratmokodani@gmail.com>
2020-05-15 07:14:14 +0700

    Praktikum Modul 6

---
+++ app/src/main/java/com/example/crudmahasiswa/Konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.crudmahasiswa;
+
+public class Konfigurasi {
+    public static final String URL_ADD="https://daniindratmoko.000webhostapp.com/cob/insert.php";
+    public static final String URL_GET_ALL="https://daniindratmoko.000webhostapp.com/cob/read.php";
+    public static final String URL_GET_MHS="https://daniindratmoko.000webhostapp.com/cob/select.php?id=";
+    public static final String URL_UPDATE_MHS="https://daniindratmoko.000webhostapp.com/cob/update.php";
+    public static final String URL_DELETE_MHS="https://daniindratmoko.000webhostapp.com/cob/delete.php?id=";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID = "mhs_id";
+}
